e2beb4595fc40ff782a9008d8fa227159ec3045e
junnie082 <junnie082@gmail.com>
2023-03-11 00:56:47 +0900

    java-histogram 첫번째 junnie082 브랜치에서 삭제함

--- java-histogram/src/main/java/org/example/domain/Histogram.java
+++
@@ -1,7 +0,0 @@
-package org.example.domain;
-
-/**
- * 히스토그램 도메인 클래스
- */
-public class Histogram {
-}
--- java-histogram/src/main/java/org/example/view/InputView.java
+++
@@ -1,7 +0,0 @@
-package org.example.view;
-
-/**
- * 입력을 담당하는 클래스
- */
-public class InputView {
-}
