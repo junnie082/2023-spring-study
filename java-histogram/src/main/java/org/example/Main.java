332545b87521060f6b950e1cbfac2392f785fdab
junnie082 <junnie082@gmail.com>
2023-03-11 13:13:34 +0900

    [FEAT] 2차 과제

--- java-histogram/src/main/java/org/example/Main.java
+++ java-histogram/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+// InputView 클래스를 가져오려면 아래처럼 package 를 import 해 와야 함. 
+import org.example.view.InputView;
+import org.example.view.OutputView; 
+import org.example.domain.Histogram;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InputView inputView = new InputView(); 
+        inputView.numberOfData();
+        inputView.eachNumber();
+        
+        OutputView outputView = new OutputView();
+        outputView.printHistogram();
+
     }
 }
--- java-histogram/src/main/java/org/example/domain/Histogram.java
+++ java-histogram/src/main/java/org/example/domain/Histogram.java
@@ -1,7 +1,26 @@
 package org.example.domain;
 
+import java.util.ArrayList;
+
 /**
  * 히스토그램 도메인 클래스
  */
 public class Histogram {
+    static ArrayList histogram = new ArrayList(); 
+
+    public void drawHistogram(ArrayList<Integer> numbers) {
+        String bar = "";
+        for (int i = 0; i < numbers.size(); i++) {
+            int index = i; 
+            bar = "";
+            for (int j = 0; j < numbers.get(index); j++) {
+                bar += "=";
+            }
+            histogram.add(bar); 
+        }
+    }
+
+    public ArrayList getHistogram() {
+        return this.histogram;
+    }
 }
